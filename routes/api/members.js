bdea31f1c297ed9338a75628571c1c3ac3dd7d06
Victor Portugal <victorvasconcellos5@hotmail.com>
2020-11-16 13:33:43 -0300

    Post and put operations

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const members = require('../../Members')
+const uuid = require('uuid');
 
 //Gets All Members
 router.get('/', (req, res) => {
@@ -20,7 +21,36 @@ router.get("/:id", (req, res) => {
 
  //Creating Member
  router.post('/', (req, res) => {
-     res.send(req.body)
+     const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email
+     }
+
+     if(!newMember.name || !newMember.email) {
+          return res.status(400).json({msg: 'Please include a name and an email'});
+     }
+     members.push(newMember);
+     res.json(members);
+
+ });
+
+ router.put('/:id', (req, res) => {
+     const found = members.some(member => member.id === parseInt(req.params.id));
+
+     if(found) {
+         const updateMember = req.body;
+         members.forEach(member => {
+             if(member.id === parseInt(req.params.id)) {
+                 member.name = updateMember.name ? updateMember.name : member.name;
+                 member.name = updateMember.email ? updateMember.email : member.email;
+             }
+             res.json({msg: 'Member updated', member, members});
+         });
+         
+     } else {
+         res.status(400).json({msg: `No member with the id of ${req.params.id} was found.`});
+     }
  })
 
  module.exports = router;
